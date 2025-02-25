d316e6634ef7f8ff1d002fd7043c2ee247b871f7
Vladimir Borovik <v.borovik@1inch.io>
2025-02-25 11:11:02 +0400

    test

--- scripts/sync-programs.ts
+++ scripts/sync-programs.ts
@@ -5,6 +5,8 @@ function main(): void {
     const root = path.join(__dirname, '..')
     const contractDir = path.join(__dirname, '../contracts/fusion-contract')
 
+    console.log({contractDir})
+
     execSync('yarn && yarn build', {
         cwd: contractDir,
         stdio: 'inherit'
